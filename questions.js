e78ae697fd34b067da0aa44dad3db02d6e9df9db
Ryan Gautier <ryangautier2@gmail.com>
2020-04-21 15:39:33 -0500

    adding questions for prompt

--- questions.js
+++ questions.js
@@ -5,9 +5,55 @@ function mainMenu() {
         {
             type: "list",
             name: "choice",
-            message: "What would you like to do?",
-            choices: ["Add employees",
-                    "View Employees"]
+            message: "What would action would you like to take?",
+            choices: ["Add",
+                "View",
+                "Update/Delete",
+                "Exit"]
+        }
+    ])
+}
+
+function addMenu() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to add?",
+            choices: ["Add department",
+                "Add role",
+                "Add employee"]
+        }
+    ])
+}
+
+function viewMenu() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to view?",
+            choices: ["View departments",
+                "View roles",
+                "View an individual employee",
+                "View employees by manager",
+                "View total utilized budget of a department"]
+        }
+    ])
+}
+function updateMenu() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to update?",
+            choices: ["Update employee role",
+                "Update employee manager",
+                "Delete department",
+                "Delete role",
+                "Delete employee",
+                ""
+            ]
         }
     ])
 }
